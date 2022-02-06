1cce3dffd3033b9c5ac2024dfc4d9e2b09d7be71
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-02-06 01:47:12 -0400

    Adds replay uploading prototype.

---
+++ pages/api/upload.php.ts
@@ -0,0 +1,86 @@
+import "reflect-metadata";
+
+import { NextApiResponse } from "next";
+import HTTPMethod from "../../shared/api/enums/HttpMethod";
+import NextApiRequestTypedBody from "../../shared/api/query/NextApiRequestTypedBody";
+import RequestHandler from "../../shared/api/request/RequestHandler";
+import Database from "../../shared/database/Database";
+import DroidRequestValidator from "../../shared/type/DroidRequestValidator";
+import { OsuDroidScore } from "../../shared/database/entities";
+import HttpStatusCode from "../../shared/api/enums/HttpStatusCodes";
+import Responses from "../../shared/api/response/Responses";
+import { differenceInSeconds } from "date-fns";
+import EnvironmentConstants from "../../shared/constants/EnvironmentConstants";
+import IHasData from "../../shared/api/query/IHasData";
+
+type body = { replayID: string } & IHasData<string>;
+
+const validate = (body: Partial<body>): body is body => {
+  return typeof body.replayID === "string" && typeof body.data === "string";
+};
+
+export default async function handler(
+  req: NextApiRequestTypedBody<body>,
+  res: NextApiResponse<string>
+) {
+  await Database.getConnection();
+
+  if (RequestHandler.endWhenInvalidHttpMethod(req, res, HTTPMethod.POST)) {
+    return;
+  }
+
+  const { body } = req;
+
+  if (
+    DroidRequestValidator.droidStringEndOnInvalidRequest(res, validate(body)) ||
+    !validate(body)
+  ) {
+    return;
+  }
+
+  const { replayID, data } = body;
+
+  const score = await OsuDroidScore.findOne(replayID, {
+    select: ["id", "date"],
+  });
+
+  if (!score) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send(Responses.FAILED("Failed to find score to upload replay."));
+    return;
+  }
+
+  if (score.replay) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send(Responses.FAILED("Score already has a replay."));
+    return;
+  }
+
+  const dateNow = new Date();
+
+  if (
+    differenceInSeconds(score.date, dateNow) >=
+    EnvironmentConstants.EDGE_FUNCTION_LIMIT_RESPONSE_TIME
+  ) {
+    /**
+     * We remove the score from the database.
+     * safety measure.
+     * we should also periodically check for scores that have the status best and didn't submit their replays.
+     */
+    await score.remove();
+
+    res.status(HttpStatusCode.BAD_REQUEST).send(Responses.FAILED("Timed out."));
+
+    return;
+  }
+
+  const replayRaw = data.slice(191).slice(undefined, -48);
+
+  score.replay = replayRaw;
+
+  await score.save();
+
+  res.status(HttpStatusCode.OK).send(Responses.SUCCESS("Replay uploaded."));
+}
