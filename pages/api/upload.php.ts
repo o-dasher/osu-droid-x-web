fce05b36e954dc4bd4e16aa50fda35760c4b4401
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-02-06 12:37:57 -0400

    Update upload.php.ts

--- pages/api/upload.php.ts
+++ pages/api/upload.php.ts
@@ -37,7 +37,7 @@ export default async function handler(
   }
 
   const { body } = req;
-  console.log(typeof body.uploadedFile);
+  console.log(Object.keys(body));
 
   if (
     DroidRequestValidator.droidStringEndOnInvalidRequest(res, validate(body)) ||
