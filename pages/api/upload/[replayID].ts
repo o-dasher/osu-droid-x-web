da441376fcdf7bfe2f5e773e25c68c4159bbb32e
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-02-07 21:46:31 -0400

    Tries to create replay viewing

---
+++ pages/api/upload/[replayID].ts
@@ -0,0 +1,59 @@
+import "reflect-metadata";
+import "../../shared/database/IncludeFirebase";
+
+import { getBlob, getStorage, ref } from "firebase/storage";
+
+import { NextApiResponse } from "next";
+import HTTPMethod from "../../../shared/api/enums/HttpMethod";
+import NextApiRequestTypedBody from "../../../shared/api/query/NextApiRequestTypedBody";
+import RequestHandler from "../../../shared/api/request/RequestHandler";
+import Database from "../../../shared/database/Database";
+import { PatchArrayAt } from "../../../shared/node/PatchArrayAt";
+import DroidRequestValidator from "../../../shared/type/DroidRequestValidator";
+import NipaaStorage from "../../../shared/database/NipaaStorage";
+import NumberUtils from "../../../shared/utils/NumberUtils";
+import HttpStatusCode from "../../../shared/api/enums/HttpStatusCodes";
+
+export default async function handler(
+  req: NextApiRequestTypedBody<unknown>,
+  res: NextApiResponse<string>
+) {
+  PatchArrayAt();
+  await Database.getConnection();
+
+  if (RequestHandler.endWhenInvalidHttpMethod(req, res, HTTPMethod.POST)) {
+    return;
+  }
+
+  const { query } = req;
+  const { replayID } = query;
+
+  if (typeof replayID !== "string") {
+    DroidRequestValidator.droidStringEndOnInvalidRequest(res, false);
+    return;
+  }
+
+  const numericID = parseInt(replayID);
+
+  if (!NumberUtils.isNumber(numericID)) {
+    DroidRequestValidator.droidStringEndOnInvalidRequest(res, false);
+    return;
+  }
+
+  const storage = getStorage();
+  const filename = NipaaStorage.ODRFilePathFromID(numericID);
+  const replayBucket = ref(storage, filename);
+
+  let replay: Blob;
+  try {
+    replay = await getBlob(replayBucket);
+  } catch {
+    res.status(HttpStatusCode.BAD_REQUEST).send("Replay not found.");
+    return;
+  }
+
+  const downloadStream = replay.stream();
+  await new Promise((resolve) => {
+    downloadStream.pipe(res), downloadStream.on("end", resolve);
+  });
+}
